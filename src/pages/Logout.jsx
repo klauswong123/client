02767f1e78e1d2250f42113a9b0450caf6bb81ec
klauswong123 <>
2021-03-11 01:17:20 +0800

    123

--- src/pages/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import Cookies from 'js-cookie'
-import { Redirect } from 'react-router'
-const Logout = () => {
-  Cookies.remove('isLogin')
-  Cookies.remove('phone_number')
-  return <Redirect to='/aic-landing'/>
-}
-
-export default Logout
